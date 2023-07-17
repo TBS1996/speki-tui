fd86978dfd6d756af433c096133ec6dff17e49d5
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-07-17 07:13:15 +0200

    wip [no ci]

--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -3,7 +3,7 @@ use std::fmt::Display;
 use std::sync::Arc;
 use std::time::Duration;
 
-use speki_backend::card::{Card, CardCache, IsSuspended, Priority, ReviewType, Reviews, SavedCard};
+use speki_backend::card::{Card, CardCache, IsSuspended, Priority, Reviews, SavedCard};
 use speki_backend::categories::Category;
 use speki_backend::common::duration_to_days;
 use speki_backend::common::{current_time, truncate_string};
@@ -12,7 +12,7 @@ use speki_backend::Id;
 
 use ascii_tree::write_tree;
 
-use rand::seq::SliceRandom;
+
 
 use crossterm::cursor::{self, MoveDown, MoveLeft};
 use crossterm::event::KeyEvent;
@@ -909,7 +909,6 @@ use std::io::{Stdout, Write};
 
 use crate::backend::{
     _get_char, cards_as_string, get_key_event, get_keycode, should_exit, to_ascii_tree,
-    CardsFromCategory,
 };
 
 pub enum SomeStatus {
--- src/pages/reviewcards.rs
+++ src/pages/reviewcards.rs
@@ -1,29 +1,27 @@
-use std::collections::{BTreeSet, HashSet};
-use std::fmt::Display;
+use std::collections::{BTreeSet};
+
 use std::io::Stdout;
-use std::sync::Arc;
+
 use std::time::Duration;
 
-use speki_backend::card::{Card, CardCache, IsSuspended, Priority, ReviewType, Reviews, SavedCard};
+use speki_backend::card::{CardCache, ReviewType, SavedCard};
 use speki_backend::categories::Category;
-use speki_backend::common::duration_to_days;
-use speki_backend::common::{current_time, truncate_string};
+
+use speki_backend::common::{current_time};
 
 use speki_backend::Id;
 
-use ascii_tree::write_tree;
+
 
 use rand::seq::SliceRandom;
 
-use crossterm::cursor::{self, MoveDown, MoveLeft};
-use crossterm::event::KeyEvent;
-use crossterm::style::Print;
-use crossterm::terminal;
+
+
+
+
 use crossterm::{
-    cursor::MoveTo,
-    event::{read, Event, KeyCode},
+    event::{KeyCode},
     execute,
-    style::{ResetColor, SetForegroundColor},
     terminal::{Clear, ClearType},
 };
 
